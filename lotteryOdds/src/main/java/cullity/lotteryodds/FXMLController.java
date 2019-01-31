f427a79706bfb3f07040e5ff257730077a5aedea
conman1005 <connercullity@live.ca>
2019-01-31 01:47:06 -0500

    Initialized the Program

---
+++ lotteryOdds/src/main/java/cullity/lotteryodds/FXMLController.java
@@ -0,0 +1,15 @@
+package cullity.lotteryodds;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class FXMLController implements Initializable {
+    
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+}
